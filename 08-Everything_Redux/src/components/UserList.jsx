54fe7c8f06f97d438f6c3c88a17c1c3f216851ec
Himanshu Chauhan <117630839+himanshu-AW@users.noreply.github.com>
2025-03-05 23:44:51 +0530

    Update UserList.jsx

--- 08-Everything_Redux/src/components/UserList.jsx
+++ 08-Everything_Redux/src/components/UserList.jsx
@@ -15,14 +15,10 @@ const deleteHandlerUser = (id) =>{
     <div className="container mx-auto bg-zinc-700 rounded-md p-6 mt-5">
       <h1 className="text-2xl text-red-500 font-bold underline">User list</h1>
       <ul>
-        {users.map((user,index) => (
+        { users.map((user,index) => (
           <li key={user.id} className="py-1 flex gap-10">
             <h1 className="text-white text-lg ">{user.name}</h1>
-            <span
-            onClick={()=>deleteHandlerUser(index)}
-            className="text-red-500 text-lg font-bold pointer">
-              X
-            </span>
+            <span onClick={()=>deleteHandlerUser(index)} className="text-red-500 text-lg font-bold pointer"> X </span>
           </li>
         ))}
       </ul>
